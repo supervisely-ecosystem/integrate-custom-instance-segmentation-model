ad3294b6b8f1fdae1e86f37b8b029d32fd1452a3
Nikolai Petukhov <petukhov.niko@gmail.com>
2022-12-08 12:44:42 -0300

    added settings example

--- src/test_inference_image_file.py
+++ src/test_inference_image_file.py
@@ -1,9 +1,13 @@
 import requests
+import json
 
 
 SERVER_URL = 'http://127.0.0.1:8000/inference_images'
 IMAGE_PATH = 'demo_data/image_01.jpg'
-SETTINGS = "{}"
+SETTINGS = json.dumps({
+    'settings': {'confidence_threshold': 0.25}
+})
+
 
 def run():
     r = requests.post(SERVER_URL, files=(
